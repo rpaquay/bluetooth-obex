942b8a6952270593996b352b1eb214fb40bcfbd4
Renaud Paquay <rpaquay@chromium.org>
2014-02-11 12:50:04 -0800

    update comment.

--- src/project.ts
+++ src/project.ts
@@ -1,3 +1,5 @@
+/// Note: This source file is used as a "project" file when opened in Visual
+/// Studio. It should not have purpose at run-time.
 /// <reference path="send_file.ts"/>
 /// <reference path="main.ts"/>
 /// <reference path="obex_test.ts"/>
